974675edaf4fca9672243c5e336c8b3491092cd5
Kalana Vimukthi Weerarathna <kalana.weerarathne@scicom.com.my>
2020-05-07 21:47:54 +0530

    initial commit

---
+++ hrate-service/src/main/java/com/tarasabella/hrateservice/HrateServiceApplication.java
@@ -0,0 +1,13 @@
+package com.tarasabella.hrateservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrateServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrateServiceApplication.class, args);
+	}
+
+}
---
+++ hrate-service/src/test/java/com/tarasabella/hrateservice/HrateServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tarasabella.hrateservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrateServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
